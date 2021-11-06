03ce86a58597ed6ac208a5af27940b1b62c2da65
Alexander Metzner <alexander.metzner@gmail.com>
2021-11-06 22:39:21 +0100

    test: fix test

--- service/src/test/java/com/github/halimath/fatecoreremotetable/boundary/ResponseSerializerTest.java
+++ service/src/test/java/com/github/halimath/fatecoreremotetable/boundary/ResponseSerializerTest.java
@@ -21,21 +21,21 @@ public class ResponseSerializerTest {
     @Test
     void shouldSerializeErrorResponse() throws JsonProcessingException {
         final var got = serializer.serialize(
-                new Response("1", Response.Type.ERROR, null, new Response.Error("1", 404, "Table not found")));
+                new Response("1", "1", Response.Type.ERROR, null, new Response.Error("1", 404, "Table not found")));
         assertEquals("""
-                {"id":"1","type":"error","error":{"requestId":"1","code":404,"reason":"Table not found"}}""", 
+                {"id":"1","self":"1","type":"error","error":{"requestId":"1","code":404,"reason":"Table not found"}}""", 
                 got);
     }
 
     @Test
     void shouldSerializeTableResponse() throws JsonProcessingException {
-        final var got = serializer.serialize(new Response("1", Response.Type.TABLE,
+        final var got = serializer.serialize(new Response("1", "1", Response.Type.TABLE,
                 new Response.Table("1", "test", "gm", List.of(new Response.Table.Player("1", "Cynere", 2, List.of())),
                         List.of(new Response.Table.Aspect("1", "Fog"))),
                 null));
 
         assertEquals("""
-            {"id":"1","type":"table","table":{"id":"1","title":"test","gamemaster":"gm","players":[{"id":"1","name":"Cynere","fatePoints":2,"aspects":[]}],"aspects":[{"id":"1","name":"Fog"}]}}""",
+            {"id":"1","self":"1","type":"table","table":{"id":"1","title":"test","gamemaster":"gm","players":[{"id":"1","name":"Cynere","fatePoints":2,"aspects":[]}],"aspects":[{"id":"1","name":"Fog"}]}}""",
             got);
     }
 }
