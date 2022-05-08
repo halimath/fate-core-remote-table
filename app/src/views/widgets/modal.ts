ed7ac7e593d2b6c9a9c85fe84889f0695c6fd582
Alexander Metzner <alexander.metzner@gmail.com>
2022-05-08 07:38:40 +0200

    feature: modal transition

--- app/src/views/widgets/modal.ts
+++ app/src/views/widgets/modal.ts
@@ -30,44 +30,57 @@ export interface ModalOptions {
 
 export function modal(opts: ModalOptions): Modal {
     const backdrop = document.createElement("div")
-    backdrop.classList.add("fixed", "inset-0", "bg-gray-500", "bg-opacity-75", "z-40", "backdrop-filter", "backdrop-blur-sm")
+    // backdrop.classList.add("modal-backdrop")
+    backdrop.classList.add("fixed", "inset-0", "bg-gray-500", "z-40", "backdrop-filter", "backdrop-blur-sm", "transition", "duration-100", "bg-opacity-0")
 
     const modalElement = document.createElement("div")
-    modalElement.classList.add("fixed", "z-50", "inset-0", "overflow-y-auto")
+    modalElement.classList.add("fixed", "z-50", "inset-0", "overflow-y-auto", "transition-opacity", "duration-200", "opacity-0")
 
     let keyboardListener: (evt: KeyboardEvent) => void
 
     const modal = {
         show() {
-            document.body.appendChild(backdrop)
-            document.body.appendChild(modalElement)
-            wecco.updateElement(modalElement, wecco.html`
-            <div class="flex items-end justify-center lg:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 mt-24">
-                <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                    <div class="bg-gray-100 px-4 py-3 sm:px-6 sm:flex"><h3>${opts.title ?? ""}</h3></div>
-                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <div class="sm:flex sm:items-start">
-                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">                            
-                                ${opts.body}
+            return new Promise<void>(resolve => {
+                document.body.appendChild(backdrop)
+                document.body.appendChild(modalElement)
+                wecco.updateElement(modalElement, wecco.html`
+                <div class="flex items-end justify-center lg:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 mt-24">
+                    <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                        <div class="bg-gray-100 px-4 py-3 sm:px-6 sm:flex"><h3>${opts.title ?? ""}</h3></div>
+                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <div class="sm:flex sm:items-start">
+                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">                            
+                                    ${opts.body}
+                                </div>
                             </div>
                         </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse border-top border-gray-100">
+                            ${(opts.actions ?? []).map(a => renderModalAction(a, modal))}
+                        </div>
                     </div>
-                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse border-top border-gray-100">
-                        ${(opts.actions ?? []).map(a => renderModalAction(a, modal))}
-                    </div>
-                </div>
-            </div>    
-            `)
-            document.addEventListener("keydown", keyboardListener)
-
-            return Promise.resolve()
+                </div>    
+                `)
+                document.addEventListener("keydown", keyboardListener)
+                backdrop.classList.add("bg-opacity-75")
+                modalElement.classList.add("opacity-100")
+                modalElement.addEventListener("transitionend", resolve.bind(null, void null), {
+                    once: true,
+                })
+            })
         },
 
         hide() {
-            document.body.removeChild(modalElement)
-            document.body.removeChild(backdrop)
-            
-            return Promise.resolve()
+            return new Promise<void>(resolve => {
+                backdrop.classList.remove("bg-opacity-75")
+                modalElement.classList.remove("opacity-100")
+                modalElement.addEventListener("transitionend", () => {
+                    document.body.removeChild(modalElement)
+                    document.body.removeChild(backdrop)
+                    resolve(void null)    
+                }, {
+                    once: true,
+                })
+            })
         },
     }
 
@@ -84,7 +97,6 @@ export function modal(opts: ModalOptions): Modal {
             }
         }
     }
-
 
     return modal
 }
